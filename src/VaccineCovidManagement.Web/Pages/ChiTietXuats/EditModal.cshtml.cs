e415aea08ba76cce4c8fe9cf51870fc021731600
quynh.tkqpy@gmail.com <quynh.tkqpy@gmail.com>
2022-08-10 17:25:11 +0700

    doing edit soluong xuat

--- src/VaccineCovidManagement.Web/Pages/ChiTietXuats/EditModal.cshtml.cs
+++ src/VaccineCovidManagement.Web/Pages/ChiTietXuats/EditModal.cshtml.cs
@@ -30,6 +30,8 @@ namespace VaccineCovidManagement.Web.Pages.ChiTietXuats
             _vaccineTonKhoAppService = vaccineTonKhoAppService;
         }
         [BindProperty]
+        public ValueOld valueOld { get; set; }
+        [BindProperty]
         public EditChiTietXuatViewModal EditChiTietXuat { get; set; }
         [BindProperty]
         public List<SelectListItem> DonViYTes { get; set; }
@@ -53,18 +55,43 @@ namespace VaccineCovidManagement.Web.Pages.ChiTietXuats
 
         public async Task<IActionResult> OnPostAsync()
         {
+            var checkId = (valueOld.VaccineIDOld == EditChiTietXuat.VaccineTonKhoID);
+            var resultChangeSoLuong = EditChiTietXuat.SoLuongXuat - valueOld.SoLuongTonKhoOld;
+            var VaccineTonKhoDto = new CreateUpdateVaccineTonKhoDto();
+            var createDonViYTe = new CreateUpdateDonViYTeDto();
+            var dvytDto = await _donViYTeAppService.GetDonViYTeAsync(EditChiTietXuat.DonViID);
             if (EditChiTietXuat.SoLuongXuat > 0)
             {
-                var VaccineTonKhoDto = new CreateUpdateVaccineTonKhoDto();
-                var vaccineDto = await _vaccineTonKhoAppService.GetVaccineTonKhoAsync(EditChiTietXuat.VaccineTonKhoID);
-                var mess = "Số vaccine " + '"' + vaccineDto.TenVaccineTonKho + '"' + " trong kho còn " + vaccineDto.SoLuongTonKho + " (liều) không đủ để xuất";
-                if (vaccineDto.SoLuongTonKho < EditChiTietXuat.SoLuongXuat)
+                switch (checkId)
                 {
-                    throw new UserFriendlyException(L[mess]);
+                    case true:
+                        var vaccineDto = await _vaccineTonKhoAppService.GetVaccineTonKhoAsync(EditChiTietXuat.VaccineTonKhoID); 
+                        var mess = "Số vaccine " + '"' + vaccineDto.TenVaccineTonKho + '"' + " trong kho còn " + vaccineDto.SoLuongTonKho + " (liều) không đủ để xuất";
+                        if (vaccineDto.SoLuongTonKho < EditChiTietXuat.SoLuongXuat)
+                        {
+                            throw new UserFriendlyException(L[mess]);
+                        }
+                        vaccineDto.SoLuongTonKho += resultChangeSoLuong;
+                        VaccineTonKhoDto.TenVaccineTonKho = vaccineDto.TenVaccineTonKho;
+                        VaccineTonKhoDto.SoLuongTonKho = vaccineDto.SoLuongTonKho;
+                        await _vaccineTonKhoAppService.UpdateAsync(vaccineDto.Id, VaccineTonKhoDto);
+                        break;
+
+                    case false:
+                        var quanLyVaccineOld = await _vaccineTonKhoAppService.GetVaccineTonKhoAsync(valueOld.VaccineIDOld);
+                        VaccineTonKhoDto.TenVaccineTonKho = quanLyVaccineOld.TenVaccineTonKho;
+                        VaccineTonKhoDto.SoLuongTonKho = quanLyVaccineOld.SoLuongTonKho - valueOld.SoLuongTonKhoOld;
+                        await _vaccineTonKhoAppService.UpdateAsync(quanLyVaccineOld.Id, VaccineTonKhoDto);
+
+                        var quanLyVaccineNew = await _vaccineTonKhoAppService.GetVaccineTonKhoAsync(EditChiTietXuat.VaccineTonKhoID);
+                        quanLyVaccineNew.SoLuongTonKho += EditChiTietXuat.SoLuongXuat;
+                        VaccineTonKhoDto.TenVaccineTonKho = quanLyVaccineNew.TenVaccineTonKho;
+                        VaccineTonKhoDto.SoLuongTonKho = quanLyVaccineNew.SoLuongTonKho;
+                        await _vaccineTonKhoAppService.UpdateAsync(quanLyVaccineNew.Id, VaccineTonKhoDto);
+                        break;
                 }
-                VaccineTonKhoDto.TenVaccineTonKho = vaccineDto.TenVaccineTonKho;
-                VaccineTonKhoDto.SoLuongTonKho = vaccineDto.SoLuongTonKho - EditChiTietXuat.SoLuongXuat;
-                await _vaccineTonKhoAppService.UpdateAsync(vaccineDto.Id, VaccineTonKhoDto);
+
+                createDonViYTe.TenDonViYTe = dvytDto.TenDonViYTe;
                 var upDateChiTiet = ObjectMapper.Map<EditChiTietXuatViewModal, CreateUpdateChiTietXuatDto>(EditChiTietXuat);
                 await _chiTietXuatAppService.UpdateAsync(EditChiTietXuat.Id, upDateChiTiet);
             }
@@ -85,6 +112,14 @@ namespace VaccineCovidManagement.Web.Pages.ChiTietXuats
             [DisplayName("Vaccine")]
             public Guid VaccineTonKhoID { get; set; }
             public int SoLuongXuat { get; set; }
+        }
+
+        public class ValueOld
+        {
+            [HiddenInput]
+            public Guid VaccineIDOld { get; set; }
+            [HiddenInput]
+            public int SoLuongTonKhoOld { get; set; }
         }
     }
 }
